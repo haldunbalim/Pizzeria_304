0b2594424ad2952108ddd2f3a9a91ba4038edd2d
haldunbalim <haldunbalim@gmail.com>
2020-03-27 02:37:16 -0700

    DataSource and ViewModel added

---
+++ src/DataSource/OrdersDataSource.java
@@ -0,0 +1,12 @@
+package DataSource;
+
+public class OrdersDataSource {
+    private static OrdersDataSource ourInstance = new OrdersDataSource();
+
+    public static OrdersDataSource getInstance() {
+        return ourInstance;
+    }
+
+    private OrdersDataSource() {
+    }
+}

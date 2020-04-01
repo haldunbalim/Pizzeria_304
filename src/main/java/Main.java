f020855287b46a4e643ada8cf3261d516bf3d99d
gsaltintas <galtintas17@ku.edu.tr>
2020-04-01 02:46:36 -0700

    Implemented automatic connection shut down

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import Service.Coordinator;
+import database.DatabaseConnectionHandler;
 
 
 public class Main {
 
     public static void main(String[] args) {
         Coordinator.getInstance().start();
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                DatabaseConnectionHandler.getInstance().close();
+            }
+        }, "Shutdown-thread"));
     }
 }
